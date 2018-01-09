3df94f5fad1670c9bf31ed6a391ff9168f9201f0
Kirill Maslov <maslovks@gmail.com>
2018-01-09 13:31:32 +0300

    first commit

--- README.md
+++ README.md
@@ -1,7 +1,48 @@
 # Sites Monitoring Utility
+The script monitors the sites for response code status and expiration date.
+It checks whether site is expiring in month, and whether http response status is 200.
 
-[TODO. There will be project description]
+To run the script python3.5 is requierd.
+Side packages are listed in requirements.txt. To install packages run the following command in shell:
+```bash
+pip install -r requirements.txt
+```
+
+The script takes one positional argument:
+* path - the path to file, containing urls of sites for monitoring
+
+## Example of file content
+```
+https://lenta.ru
+https://www.championat.com
+https://devman.org
+https://yandex.ru
+```
+
+# How to run the script
+```
+$ python check_sites_health.py './sites.txt' 
+**************************************************
+Checking https://lenta.ru
+	Server respond with 200: True
+	Expiring in month: False
+
+**************************************************
+Checking https://www.championat.com
+	Server respond with 200: True
+	Expiring in month: False
+
+**************************************************
+Checking https://devman.org
+	Server respond with 200: True
+	Expiring in month: False
+
+**************************************************
+Checking https://yandex.ru
+	Server respond with 200: True
+	Expiring in month: False
+
+```
 
 # Project Goals
-
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+The code is written for educational purposes. Training course for web-developers - DEVMAN.org
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,54 @@
+import whois
+import requests
+from datetime import datetime
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help='Path to domains file')
+    args = parser.parse_args()
+    return args.path
+
+
 def load_urls4check(path):
-    pass
+    urls = []
+    with open(path, 'r') as file:
+        for line in file:
+            urls.append(line.rstrip())
+    return urls
+
 
 def is_server_respond_with_200(url):
-    pass
+    request = requests.get(url)
+    if request.status_code == 200:
+        return True
+    else:
+        return False
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def is_expiration_in_month(date_time):
+    if isinstance(date_time, list):
+        time_left = min(date_time) - datetime.now()
+    else:
+        time_left = date_time - datetime.now()
+    return True if time_left.days < 31 else False
+
+
+def get_domain_expiration_date(url):
+    whois_obj = whois.whois(url)
+    return whois_obj.expiration_date
+
 
 if __name__ == '__main__':
-    pass
+    domains_file_path = parse_arguments()
+    sites_to_check = load_urls4check(domains_file_path)
+    for site_url in sites_to_check:
+        print('*' * 50)
+        print('Checking {}'.format(site_url))
+        print('\tServer respond with 200: {}'.format(
+            is_server_respond_with_200(site_url)))
+
+        expiration_date = get_domain_expiration_date(site_url)
+        print('\tExpiring in month: {}\n'.format(
+            is_expiration_in_month(expiration_date)))
